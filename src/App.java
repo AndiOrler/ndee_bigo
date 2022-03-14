9534b4622c3fd81d6eb3ac971ff2337d29bd6a7b
AndiOrler <andreas.orler@gmail.com>
2022-03-14 21:59:45 +0100

    const complexity added

--- src/App.java
+++ src/App.java
@@ -1,5 +1,31 @@
+import java.util.LinkedList;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("hello world");
+
+        // O(1)
+        constComplexity();
+
+
+    }
+
+    static LinkedList<Integer> createList(int n) {
+        LinkedList<Integer> li = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            li.add(i);
+        }
+        return li;
+    }
+
+    static void constComplexity() {
+        for (int n = 32; n <= 8388608; n *= 2) {
+            LinkedList<Integer> list = createList(n);
+
+            long time = System.nanoTime();
+            list.add(0, 1);
+            time = System.nanoTime() - time;
+
+            System.out.printf("n = %d -> time = %d ns%n", n, time);
+        }
     }
 }
