3526fec2c631cb78b657012b0d4efc576c0d630b
AndiOrler <andreas.orler@gmail.com>
2022-03-14 22:17:48 +0100

    linear complexity added

--- src/App.java
+++ src/App.java
@@ -4,18 +4,16 @@ public class App {
     public static void main(String[] args) {
 
         // O(1)
+        // not always exactly the same but spread over all iterations
         constComplexity();
 
+        //O(n)
+        //steadily increasing
+        linearComplexity();
 
     }
 
-    static LinkedList<Integer> createList(int n) {
-        LinkedList<Integer> li = new LinkedList<>();
-        for (int i = 0; i < n; i++) {
-            li.add(i);
-        }
-        return li;
-    }
+
 
     static void constComplexity() {
         for (int n = 32; n <= 8388608; n *= 2) {
@@ -27,5 +25,36 @@ public class App {
 
             System.out.printf("n = %d -> time = %d ns%n", n, time);
         }
+    }
+
+    static void linearComplexity() {
+        for (int n = 32; n <= 8388608; n *= 2){
+            int[] ar = createArray(n);
+
+            long sum = 0;
+            long time = System.nanoTime();
+            for (int i = 0; i < n; i++) {
+                sum += ar[i];
+            }
+            time = System.nanoTime() - time;
+
+            System.out.printf("n = %d -> time = %d ns%n", n, time);
+        }
+    }
+
+    static LinkedList<Integer> createList(int n) {
+        LinkedList<Integer> li = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            li.add(i);
+        }
+        return li;
+    }
+
+    static int[] createArray(int n) {
+        int[] ar = new int[n];
+        for (int i = 0; i < n; i++) {
+            ar[i] = i;
+        }
+        return ar;
     }
 }
